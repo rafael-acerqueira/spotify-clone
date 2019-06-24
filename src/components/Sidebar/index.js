4a94753eae6d73eb2e522a1cb5cf6031e8f44f35
Rafael <rafael.acerqueirah@gmail.com>
2019-06-24 10:31:09 -0300

    Add more items and styles to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,59 @@
 import React from 'react'
 
-import { Container, NewPlayList, AddPlayListImage } from './styles'
+import { Container, NewPlayList, AddPlayListImage, Nav } from './styles'
 
 const Sidebar = () => (
 	<Container>
-		<div />
+		<div>
+			<Nav main>
+				<li>
+					<a href="">Navegar</a>
+				</li>
+				<li>
+					<a href="">Rádio</a>
+				</li>
+			</Nav>
+			<Nav>
+				<li>
+					<span>Sua Biblioteca</span>
+				</li>
+				<li>
+					<a href="">Seu Daily Mix</a>
+				</li>
+				<li>
+					<a href="">Tocados Recentemente</a>
+				</li>
+				<li>
+					<a href="">Músicas</a>
+				</li>
+				<li>
+					<a href="">Albúns</a>
+				</li>
+				<li>
+					<a href="">Artistas</a>
+				</li>
+				<li>
+					<a href="">Estações</a>
+				</li>
+				<li>
+					<a href="">Arquivos Locais</a>
+				</li>
+				<li>
+					<a href="">Vídeos</a>
+				</li>
+				<li>
+					<a href="">Podcasts</a>
+				</li>
+			</Nav>
+			<Nav>
+				<li>
+					<span>Playlists</span>
+				</li>
+				<li>
+					<a href="">Arrocha mamãe</a>
+				</li>
+			</Nav>
+		</div>
 		<NewPlayList>
 			<AddPlayListImage />
 			Nova Playlist
