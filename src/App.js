2d03386d2f8f660f312d5a4e415b6b3d73fa23c3
Rafael <rafael.acerqueirah@gmail.com>
2019-06-27 09:48:01 -0300

    Configure Rectotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
-
+import './config/reactotron'
 import GlobalStyle from './syles/global'
 
 import Sidebar from './components/Sidebar'
