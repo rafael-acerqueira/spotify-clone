b0420e88a4f3b06dfa54ea6b3dbf34b837992621
Rafael <rafael.acerqueirah@gmail.com>
2019-06-28 09:16:26 -0300

    Create loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SpinnerIcon from '../../assets/images/loading.svg'
+
+import { Spinner } from './styles'
+
+const Loading = () => <Spinner src={SpinnerIcon} alt="carregando" />
+
+export default Loading
