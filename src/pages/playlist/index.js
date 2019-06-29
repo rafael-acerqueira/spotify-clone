4bc8b3dda0ee9f86182de795d1a0d78630fd7ce2
Rafael <rafael.acerqueirah@gmail.com>
2019-06-29 10:24:25 -0300

    Connect Playlist Page to Redux

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,124 +1,81 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails'
 
 import { Container, Header, SongList } from './styles'
 
 import ClockIcon from '../../assets/images/clock.svg'
 import AddIcon from '../../assets/images/plus.svg'
 
-const Playlist = () => (
-	<Container>
-		<Header>
-			<img
-				src="http://portaldoguigui.com.br/wp-content/uploads/2018/08/37500557_2090114051047574_3731377190588121088_n-300x300.jpg"
-				alt=""
-			/>
+import Loading from '../../components/Loading'
 
-			<div>
-				<span>Playlist</span>
-				<h1>Playlist name</h1>
-				<p>25 songs</p>
+const Playlist = ({ match }) => {
+	const playlistDetails = useSelector(state => state.playlistDetails)
+	const dispatch = useDispatch()
 
-				<button>Play</button>
-			</div>
-		</Header>
+	useEffect(() => {
+		dispatch(PlaylistDetailsActions.getPlaylistDetailsRequest(match.params.id))
+	}, [match.params.id])
 
-		<SongList cellPadding={0} cellSpacing={0}>
-			<thead>
-				<tr>
-					<th />
-					<th>Título</th>
-					<th>Artista</th>
-					<th>Álbum</th>
-					<th>
-						<img src={ClockIcon} alt="duracao" />
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>{' '}
-				<tr>
-					<td>
-						<img src={AddIcon} alt="adicionar" />
-					</td>
-					<td>Renascerá</td>
-					<td>Calcinha Preta</td>
-					<td>DVD 2 Belém do Pará</td>
-					<td>2:21</td>
-				</tr>
-			</tbody>
-		</SongList>
-	</Container>
-)
+	const renderDetails = () => {
+		const playlist = playlistDetails.data
+		return (
+			<Container>
+				<Header>
+					<img src={playlist.thumbnail} alt={playlist.title} />
+
+					<div>
+						<span>Playlist</span>
+						<h1>{playlist.title}</h1>
+						{!!playlist.songs && <p>{playlist.songs.length} músicas</p>}
+
+						<button>Play</button>
+					</div>
+				</Header>
+
+				<SongList cellPadding={0} cellSpacing={0}>
+					<thead>
+						<tr>
+							<th />
+							<th>Título</th>
+							<th>Artista</th>
+							<th>Álbum</th>
+							<th>
+								<img src={ClockIcon} alt="duracao" />
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{!playlist.songs ? (
+							<tr>
+								<td colSpan={5}>Nenhuma música nessa playlist</td>
+							</tr>
+						) : (
+							playlist.songs.map(song => (
+								<tr key={song.id}>
+									<td>
+										<img src={AddIcon} alt="adicionar" />
+									</td>
+									<td>{song.title}</td>
+									<td>{song.author}</td>
+									<td>{song.album}</td>
+									<td>2:21</td>
+								</tr>
+							))
+						)}
+					</tbody>
+				</SongList>
+			</Container>
+		)
+	}
+	return playlistDetails.loading ? (
+		<Container loading>
+			<Loading />
+		</Container>
+	) : (
+		renderDetails()
+	)
+}
 
 export default Playlist
